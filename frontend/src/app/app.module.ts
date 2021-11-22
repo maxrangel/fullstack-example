eb0f9e69519e832afeeb30cb6bb86e6dea683b71
maxrangel <lik4ever@hotmail.com>
2021-11-22 00:12:01 -0600

    added login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -8,10 +9,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		AuthModule.forRoot({
+			domain: 'dev-41kwqg9q.us.auth0.com',
+			clientId: 'injO8EX66B9FEHaxpKmWvn5taxPosOJR',
+		}),
+	],
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
